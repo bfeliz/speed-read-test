2d2c7996571b9bb87065a14f0e9a12090343325d
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-04-30 15:09:38 -0700

    Add scoring functionality

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ const words = text.split(" ");
 const countdown = document.querySelector(".countdown");
 const display = document.querySelector(".text");
 
+let timers = [];
 let timer = 5;
 let i = 0;
+let stop = false;
 
 function prepareRead() {
     const timerInterval = setInterval(function () {
@@ -19,6 +21,46 @@ function prepareRead() {
             speedRead();
         }
     }, 1000);
+}
+
+function finalScore() {
+    switch (true) {
+        case i > 0 && i <= 15:
+            display.textContent = "Your final score is: 60 words per minute ";
+            break;
+        case i > 15 && i <= 30:
+            display.textContent = "Your final score is: 120 words per minute ";
+            break;
+        case i > 30 && i <= 50:
+            display.textContent = "Your final score is: 160 words per minute ";
+            break;
+        case i > 50 && i <= 75:
+            display.textContent = "Your final score is: 240 words per minute ";
+            break;
+        case i > 75 && i <= 105:
+            display.textContent = "Your final score is: 320 words per minute ";
+            break;
+        case i > 105 && i <= 140:
+            display.textContent = "Your final score is: 480 words per minute ";
+            break;
+        case i > 140 && i <= 179:
+            display.textContent = "Your final score is: 640 words per minute ";
+            break;
+        case i === 180:
+            display.textContent =
+                "You aced the test! Your final score is: 960 words per minute ";
+            break;
+    }
+    stop = true;
+}
+
+function beginAgain() {
+    display.textContent = " ";
+    countdown.textContent = "5 seconds left";
+    timer = 5;
+    i = 0;
+    stop = false;
+    prepareRead();
 }
 
 function speedRead() {
@@ -64,57 +106,106 @@ function speedRead() {
                                                                                 clearInterval(
                                                                                     timeEight
                                                                                 );
+                                                                                finalScore();
+                                                                            } else if (
+                                                                                stop ===
+                                                                                true
+                                                                            ) {
+                                                                                clearInterval(
+                                                                                    timeEight
+                                                                                );
                                                                             } else {
-                                                                                display.textContent =
-                                                                                    words[
-                                                                                        i
-                                                                                    ];
-                                                                                i++;
+                                                                                if (
+                                                                                    stop ===
+                                                                                    false
+                                                                                ) {
+                                                                                    display.textContent =
+                                                                                        words[
+                                                                                            i
+                                                                                        ];
+                                                                                    i++;
+                                                                                }
                                                                             }
                                                                         },
                                                                         62.5
                                                                     );
+                                                                } else if (
+                                                                    stop ===
+                                                                    true
+                                                                ) {
+                                                                    clearInterval(
+                                                                        timeSeven
+                                                                    );
                                                                 } else {
-                                                                    display.textContent =
-                                                                        words[
-                                                                            i
-                                                                        ];
-                                                                    i++;
+                                                                    if (
+                                                                        stop ===
+                                                                        false
+                                                                    ) {
+                                                                        display.textContent =
+                                                                            words[
+                                                                                i
+                                                                            ];
+                                                                        i++;
+                                                                    }
                                                                 }
                                                             },
                                                             93.75
                                                         );
+                                                    } else if (stop === true) {
+                                                        clearInterval(timeSix);
                                                     } else {
-                                                        display.textContent =
-                                                            words[i];
-                                                        i++;
+                                                        if (stop === false) {
+                                                            display.textContent =
+                                                                words[i];
+                                                            i++;
+                                                        }
                                                     }
                                                 },
                                                 125
                                             );
+                                        } else if (stop === true) {
+                                            clearInterval(timeFive);
                                         } else {
-                                            display.textContent = words[i];
-                                            i++;
+                                            if (stop === false) {
+                                                display.textContent = words[i];
+                                                i++;
+                                            }
                                         }
                                     }, 187.5);
+                                } else if (stop === true) {
+                                    clearInterval(timeFour);
                                 } else {
-                                    display.textContent = words[i];
-                                    i++;
+                                    if (stop === false) {
+                                        display.textContent = words[i];
+                                        i++;
+                                    }
                                 }
                             }, 250);
+                        } else if (stop === true) {
+                            clearInterval(timeThree);
                         } else {
-                            display.textContent = words[i];
-                            i++;
+                            if (stop === false) {
+                                display.textContent = words[i];
+                                i++;
+                            }
                         }
                     }, 375);
+                } else if (stop === true) {
+                    clearInterval(timeTwo);
                 } else {
-                    display.textContent = words[i];
-                    i++;
+                    if (stop === false) {
+                        display.textContent = words[i];
+                        i++;
+                    }
                 }
             }, 500);
+        } else if (stop === true) {
+            clearInterval(timeOne);
         } else {
-            display.textContent = words[i];
-            i++;
+            if (stop === false) {
+                display.textContent = words[i];
+                i++;
+            }
         }
     }, 1000);
 }
